66ca80c144200e27c66d18a7ce5e374845e8ef66
Pedro Mariano <mariano.pedro@gmail.com>
2018-11-10 16:10:29 +0000

    Added an interface that represents background music.

---
+++ com/anabivirtual/story/core/BackgroundMusic.java
@@ -0,0 +1,87 @@
+package com.anabivirtual.story.core;
+
+/**
+ * Music that is played in the background.
+ *
+ * <p>
+ * The music that is played depends on the user location. The music is played if
+ * the user in inside a circle. This circle is characterised by its center
+ * latitude and longitude and its radius.
+ *
+ * @author pedro
+ */
+public interface BackgroundMusic
+{
+	/**
+	 * Get the identification of this background music.
+	 * <p>
+	 * This is the primary key.
+	 *
+	 * @return the identification of this background music.
+	 */
+	long getID ();
+
+	/**
+	 * Get the audio filename of this music.
+	 *
+	 * @return the audio filename of this music.
+	 */
+	String getFilename ();
+
+	/**
+	 * Set the audio filename of this music.
+	 *
+	 * @param filename the new audio filename of this music.
+	 */
+	void setFilename (String filename);
+
+	/**
+	 * Get the latitude of the center of the region where the music is played.
+	 * Units are degree.
+	 *
+	 * @return the latitude of the center of the region where the music is
+	 * played.
+	 */
+	public double getRegionCenterLatitude ();
+
+	/**
+	 * Set the latitude of the center of the region where the music is played.
+	 * Units are degree.
+	 *
+	 * @param latitude the new latitude of the center of the region where the
+	 * music is played.
+	 */
+	public void setRegionCenterLatitude (double latitude);
+
+	/**
+	 * Get the longitude of the center of the region where the music is played..
+	 * Units are degree.
+	 *
+	 * @return the longitude of the center of the region where the music is
+	 * played.
+	 */
+	public double getRegionCenterLongitude ();
+
+	/**
+	 * Set the longitude of the center of the region where the music is played.
+	 * Units are degree.
+	 *
+	 * @param longitude the new longitude of the center of the region where the
+	 * music is played.
+	 */
+	public void setRegionCenterLongitude (double longitude);
+
+	/**
+	 * Get the radius of the region where the music is played.
+	 *
+	 * @return the radius of the region where the music is played.
+	 */
+	public double getRegionRadius ();
+
+	/**
+	 * Set the radius of the region where the music is played.
+	 *
+	 * @param radius the new radius of the region where the music is played.
+	 */
+	public void setRegionRadius (double radius);
+}
